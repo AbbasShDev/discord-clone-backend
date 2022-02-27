029a7ebaa3d4a64b0874da401ff9b08159e823cd
abbasShDev <abbas20alzaeem@gmail.com>
2022-02-27 17:15:08 +0300

    Add auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const auth = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      const user = await User.findOne({
+        _id: decoded.id,
+        "tokens.token": token,
+      }).select("-password");
+
+      if (user) {
+        req.user = user;
+        req.token = token;
+      } else {
+        throw new Error();
+      }
+
+      next();
+    } catch (error) {
+      res.status(401).json({ error: "Not authorized, token failed" });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ error: "Not authorized, token failed" });
+  }
+};
+
+module.exports = { auth };
--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,10 @@ const {
   loginValidator,
   registerValidator,
 } = require("../middleware/validator");
+const { auth } = require("../middleware/authMiddleware");
 
 router.post("/register", registerValidator, register);
 router.post("/login", loginValidator, login);
-router.post("/logout", logout);
+router.post("/logout", auth, logout);
 
 module.exports = router;
